573e1097c172dfacec8f0fbeccb3b9c87145552a
Gautam Sisodia <gautam.sisodia@gmail.com>
2020-11-09 11:41:46 -0500

    added download, extract_zip

--- pygsutils/general.py
+++ pygsutils/general.py
@@ -1,6 +1,9 @@
 # general utilities
 
 import logging
+import requests
+import os
+import zipfile
 
 
 def setup_logging(fp: str) -> None:
@@ -21,3 +24,19 @@ def setup_logging(fp: str) -> None:
     consoleHandler = logging.StreamHandler()
     consoleHandler.setFormatter(formatter)
     rootLogger.addHandler(consoleHandler)
+
+    rootLogger.setLevel(logging.DEBUG)
+
+def download(url:str, fp:str) -> None:
+    with requests.get(url) as r:
+        with open(fp, "wb") as f:
+            f.write(r.content)    
+
+def extract_zip(path_to_zip, dest_folder):
+    filename = path_to_zip.split("/")[-1]
+    foldername = filename.split(".")[0]
+    folderpath = f"{dest_folder}/{foldername}"
+    if not os.path.exists(folderpath):
+        os.makedirs(folderpath)
+    with zipfile.ZipFile(path_to_zip, "r") as zip_ref:
+        zip_ref.extractall(folderpath)
