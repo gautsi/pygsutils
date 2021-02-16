09623bef97a5801073c21f2a1440d40225ea1499
Gautam Sisodia <gautam.sisodia@gmail.com>
2021-02-16 14:33:38 -0500

    Update documentation

--- _sources/content/caching/test.py
+++ _sources/content/caching/test.py
@@ -1,30 +1,45 @@
 # %% [markdown]
 """
-# Testing diskcache
+# Testing cache
 """
 
 # %% tags=['hide-cell']
 from IPython import get_ipython
+
 if get_ipython() is not None:
-    get_ipython().run_line_magic('load_ext', 'autoreload')
-    get_ipython().run_line_magic('autoreload', '2')
+    get_ipython().run_line_magic("load_ext", "autoreload")
+    get_ipython().run_line_magic("autoreload", "2")
 
 
 # %%
-import diskcache as d
+from pygsutils import cache as c
 import pandas as pd
 
 # %%
-cache = d.Cache("tmp")
+class TestProcess:
+    cache = c.DFCache(loc="./tmp")
+
+    def __init__(self):
+        self.df = pd.DataFrame([{"a": 3}])
+
+    @property
+    @cache.store(name="df_first")
+    def df_first(self) -> pd.DataFrame:
+        print("in df_first")
+        return self.df.assign(b=1)
+
+    @property
+    @cache.store(name="df_second")
+    def df_second(self) -> pd.DataFrame:
+        print("in df_second")
+        return self.df_first.assign(c=5)
+
 
 # %%
-df = pd.DataFrame([{"a": 1}])
+tf = TestProcess()
 
 # %%
-cache["test_df"] = df
+tf.df_second
 
 # %%
-"test_df" in cache
-
-# %%
-cache.get("test_df", read=True)
+tf.df_second
