8d002730f30cc6cedf4b20490e21d5c5837e376d
Gautam Sisodia <gautam.sisodia@gmail.com>
2021-03-07 19:53:45 -0500

    upd to cache

--- pygsutils/cache.py
+++ pygsutils/cache.py
@@ -1,32 +1,81 @@
 """tools for caching, particularly pandas dataframes
 """
 from pygsutils import general as g
-from typing import Callable
+from typing import Callable, Optional, Dict, Union, List
 import pandas as pd
-from functools import wraps
+from functools import wraps, cached_property
 import os
 import logging
+import json
+from dataclasses import dataclass
+
+@dataclass
+class Spec:
+    loc: Optional[str] = None
+
+spec = Spec()
+
+def set_loc(new_loc: str) -> None:
+    spec.loc = new_loc
 
 
-class DFCache:
-    def __init__(self, loc: str):
-        self.loc = loc
-        g.make_dir(self.loc)
+class Cache:
+    def __init__(self, InputType: type, ext: str, name: str):
+        assert spec.loc is not None, "loc not set"
+        g.make_dir(spec.loc)
+        self.InputType = InputType
+        self.ext = ext
+        self.name = name
 
-    def store(self, name: str) -> Callable[..., pd.DataFrame]:
+    @property
+    def fp(self) -> str:
+        return f"{spec.loc}/{self.name}.{self.ext}"
 
-        fp = f"{self.loc}/{name}.csv"
+    @cached_property
+    def read(self):  # -> self.InputType
+        pass
 
-        def inner(func: Callable[..., pd.DataFrame]) -> Callable[..., pd.DataFrame]:
-            @wraps(func)
-            def wrapper(*args, **kwargs) -> pd.DataFrame:
-                if os.path.isfile(fp):
-                    logging.info(f"reading from cache at {fp}")
-                    df = pd.read_csv(fp, dtype=object)
-                else:
-                    df = func(*args, **kwargs)
-                    logging.info(f"saving to cache at {fp}")
-                    df.to_csv(fp, index=False)
-                return df
-            return wrapper
-        return inner
+    def write(self, obj) -> None:  # obj has type self.InputType
+        pass
+
+    def __call__(self, func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs) -> self.InputType:
+            if os.path.isfile(self.fp):
+                logging.info(f"reading from cache at {self.fp}")
+                result = self.read
+            else:
+                result = func(*args, **kwargs)
+                logging.info(f"saving to cache at {self.fp}")
+                self.write(result)
+            return result
+
+        return wrapper
+
+
+class DFCache(Cache):
+    def __init__(self, name: str):
+        super().__init__(InputType=pd.DataFrame, ext="csv", name=name)
+
+    @property
+    def read(self) -> pd.DataFrame:
+        return pd.read_csv(self.fp, dtype=object)
+
+    def write(self, obj: pd.DataFrame) -> None:
+        obj.to_csv(self.fp, index=False)
+
+
+JSONType = Union[List, Dict]
+
+
+class JSONCache(Cache):
+    def __init__(self, name: str):
+        super().__init__(InputType=JSONType, ext="json", name=name)
+
+    @property
+    def read(self) -> JSONType:
+        return json.load(self.fp)
+
+    def write(self, obj: JSONType) -> None:
+        with open(self.fp, "w") as f:
+            json.dump(obj, f)
--- pygsutils/general.py
+++ pygsutils/general.py
@@ -8,7 +8,7 @@ from functools import wraps
 from typing import Union, Callable
 
 
-def setup_logging(fp: str) -> None:
+def setup_logging(fp: str, level: int = logging.DEBUG) -> None:
     """Sets up logging to file at fp and to stdout
 
     Arguments:
@@ -27,18 +27,21 @@ def setup_logging(fp: str) -> None:
     consoleHandler.setFormatter(formatter)
     rootLogger.addHandler(consoleHandler)
 
-    rootLogger.setLevel(logging.DEBUG)
+    rootLogger.setLevel(level)
 
-def download(url:str, fp:str) -> None:
+
+def download(url: str, fp: str) -> None:
     logging.info(f"downloading {url} to {fp}")
     with requests.get(url) as r:
         with open(fp, "wb") as f:
-            f.write(r.content)    
+            f.write(r.content)
 
-def extract_zip(path_to_zip:str, dest_folder:str) -> None:
+
+def extract_zip(path_to_zip: str, dest_folder: str) -> None:
     logging.info(f"extracting {path_to_zip} to {dest_folder}")
     with zipfile.ZipFile(path_to_zip, "r") as zip_ref:
         zip_ref.extractall(dest_folder)
+
 
 def log_exception(func):
     @wraps(func)
@@ -46,21 +49,25 @@ def log_exception(func):
         try:
             return func(*args, **kwargs)
         except:
-            logging.exception(20*"-")
+            logging.exception(20 * "-")
             raise
+
     return wrapper
 
-def make_dir(dir_or_func:Union[str, Callable]) -> Union[None, Callable]:
+
+def make_dir(dir_or_func: Union[str, Callable]) -> Union[None, Callable]:
     if type(dir_or_func) is str:
         if not os.path.exists(dir_or_func):
             os.makedirs(dir_or_func)
     elif callable(dir_or_func):
+
         @wraps(dir_or_func)
         def wrapper(*args, **kwargs):
             dir = dir_or_func(*args, **kwargs)
             if not os.path.exists(dir):
                 os.makedirs(dir)
             return dir
+
         return wrapper
     else:
-        raise(f"dir_or_func type not handled: {type(dir_or_func)}")
+        raise (f"dir_or_func type not handled: {type(dir_or_func)}")
